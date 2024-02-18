cc4a5b2d26e32782d87ca67781bc5c17a3e61a4b
jnovak96 <johnwilliamnovak@gmail.com>
2024-02-18 03:27:08 -0500

    added more functionality

--- VertexBaking.py
+++ VertexBaking.py
@@ -26,6 +26,7 @@ uv_lm.active = True
 bpy.ops.object.editmode_toggle()
 bpy.ops.mesh.select_all(action='SELECT') # for all faces
 bpy.ops.uv.lightmap_pack(PREF_CONTEXT='SEL_FACES', PREF_PACK_IN_ONE=True, PREF_NEW_UVLAYER=False, PREF_BOX_DIV=12, PREF_MARGIN_DIV=0.1)
+bpy.ops.object.editmode_toggle()
 
 #add nodes to materials
 img_node = m.node_tree.nodes.new("ShaderNodeTexImage")
@@ -35,6 +36,7 @@ img_node.image = image
 uv_node.uv_map = "UV_lightmap"
 
 #bake lightmap
+D.scenes["Scene"].render.bake.target = 'IMAGE_TEXTURES'
 D.scenes["Scene"].cycles.samples = env_samples
 D.scenes["Scene"].cycles.bake_type = 'DIFFUSE'
 D.scenes["Scene"].render.bake.use_pass_direct = True
@@ -44,3 +46,29 @@ D.scenes["Scene"].render.bake.margin = margin
 D.scenes["Scene"].render.bake.use_clear = False
 D.scenes["Scene"].display_settings.display_device = 'sRGB'
 bpy.ops.object.bake(type = 'DIFFUSE')
+
+
+#add vertex attribute node and prepare for bake
+bpy.ops.geometry.color_attribute_add(name="baked_colors")
+color_attr_node = m.node_tree.nodes.new("ShaderNodeVertexColor")
+color_attr_node.layer_name = "baked_colors"
+m.node_tree.nodes.active = color_attr_node
+m.node_tree.links.new(img_node.outputs["Color"], bpy.data.materials['Material'].node_tree.nodes['Material Output'].inputs['Surface'])
+
+#bake vertex colors
+D.scenes["Scene"].cycles.samples = env_samples
+D.scenes["Scene"].cycles.bake_type = 'EMIT'
+D.scenes["Scene"].render.bake.use_pass_direct = True
+D.scenes["Scene"].render.bake.use_pass_indirect = True
+D.scenes["Scene"].render.bake.use_pass_color = False
+D.scenes["Scene"].render.bake.margin = margin
+D.scenes["Scene"].render.bake.target = 'VERTEX_COLORS'
+D.scenes["Scene"].display_settings.display_device = 'sRGB'
+bpy.ops.object.bake(type = 'EMIT')
+
+#remove nodes
+#bpy.data.materials['Material'].node_tree.nodes.remove(img_node)
+bpy.data.materials['Material'].node_tree.nodes.remove(uv_map)
+bpy.data.images.remove(image)
+o.data.uv_textures.remove('UV_lightmap')
+#other cleanup
